8d79fb453c80088c65e202db0d19045c74f96078
Mistaki <natalia.greszta@vp.pl>
2023-02-23 21:34:59 +0100

    firt screen list

--- lib/photos_screen.dart
+++ lib/photos_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -6,52 +8,54 @@ import 'package:http/http.dart' as http;
 import 'package:zadanie_rekrutacyjne/photos_from_API.dart';
 
 class PhotosScreen extends StatefulWidget {
-   const PhotosScreen({Key? key}) : super(key: key);
+  const PhotosScreen({Key? key}) : super(key: key);
 
   @override
   State<PhotosScreen> createState() => _PhotosScreenState();
 }
 
 class _PhotosScreenState extends State<PhotosScreen> {
-   Future<List<Photo>> fetchPhotos(http.Client client) async {
-     final response = await client
-         .get(Uri.parse('https://jsonplaceholder.typicode.com/photos'));
+  Future<List<Photo>> fetchPhotos(http.Client client) async {
+    try {
+      final response = await client
+          .get(Uri.parse('https://jsonplaceholder.typicode.com/photos'));
 
-       return compute(parsePhotos, response.body) as List<Photo>;
+      final list = json.decode(response.body) as List;
+      final parsedList = list.map((model) {
+        return Photo.fromJson(model);
+      }).toList();
+      return parsedList;
+    } catch (e) {
+      print(e);
+      return [];
+    }
+  }
 
-     }
-   @override
-   void initState() {
-     super.initState();
-     fetchPhotos(http.Client());
-   }
-   late List<Photo> photos = fetchPhotos(http.Client()) as List<Photo>;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: FutureBuilder<List<Photo>>(
-          future: fetchPhotos(http.Client()),
-          builder: (context, snapshot) {
-            if (snapshot.hasError) {
-              return const Center(
-                child: Text('An error has occurred!'),
-              );
-            } else if (snapshot.hasData) {
-              return GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                ),
-                itemCount: photos.length,
-                itemBuilder: (context, index) {
-                  return Image.network(photos[index].thumbnailUrl);
-                },
-              );
-            } else {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-          }
-        ));
+            future: fetchPhotos(http.Client()),
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return const Center(
+                  child: Text('An error has occurred!'),
+                );
+              } else if (snapshot.hasData) {
+                return GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                  ),
+                  itemCount: snapshot.data?.length ?? 0,
+                  itemBuilder: (context, index) {
+                    return Image.network(snapshot.data![index].thumbnailUrl);
+                  },
+                );
+              } else {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            }));
   }
 }
